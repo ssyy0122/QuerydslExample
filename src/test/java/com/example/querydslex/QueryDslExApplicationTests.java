d4a9d9b67d93139616fef444b6bd26c2073df95c
ssyy0122 <z01039118740@gmail.com>
2022-12-10 13:29:47 +0900

    프로젝트 생성

---
+++ src/test/java/com/example/querydslex/QueryDslExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.querydslex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryDslExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
